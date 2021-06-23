75ea11d44250d024a7f99784fe02dcd1a99f690d
Grecia Guerrero <greciaguerreroa@gmail.com>
2021-06-23 15:37:53 -0500

    add functions

--- src/components/currency/Currency.js
+++ src/components/currency/Currency.js
@@ -8,6 +8,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Modal from '../../shared/Modal';
 
+import * as functions from '../../utils/functions';
+
+
 const Currency = () => {
   const [open, setOpen] = useState(false); //needed to open modal
   const history = useHistory();
@@ -18,6 +21,27 @@ const Currency = () => {
   }]);
   const { profile } = useSelector((state) => state.firebase);
   const { currencies } = useSelector((state) => state.firestore.data);
+  
+  /***/
+  const {infoReceipt,costsReceipt} = useSelector((state)=>state);
+
+  let discount_date = "2021-05-05", payment_date = "2021-06-06";
+  let diff = functions.calcularDiasTranscurridos(discount_date, payment_date);//dias+1
+  console.log(`#dias ${diff}`);
+  let rate_term = 30;
+  let rate_value = 17.49;
+  let tasaANDias = functions.calcularTasaEfectivaANDiasDeEfectiva(rate_term,diff,rate_value);
+  console.log(`TE% ${tasaANDias.toFixed(7)}`);
+  let tasaDcto = functions.calcularTasaEfectivaDescuentoANDias(tasaANDias);
+  console.log(`d% ${tasaDcto.toFixed(7)}`);
+  let nominal_value = 4548;
+  let dcto = functions.calcularDescuentoANDias(tasaDcto,nominal_value);
+  console.log(`Descuento ${dcto.toFixed(2)}`);
+  let vneto = functions.calcularValorNeto(nominal_value,dcto);
+  console.log(`Valor neto ${vneto.toFixed(2)}`);
+  let retencion = 550; //ToDo: cuanto y apartir de qué monto
+  /***/
+
 
   if (!auth.uid) {
     return <Redirect to="/login" />;
@@ -38,7 +62,6 @@ const Currency = () => {
   const handleClose = () => { //needed to open modal
     setOpen(false);
   };
-
   return (
     <div>
       { !profile.isEmpty ?
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -6,3 +6,68 @@ export const newDate = () => {
 
     return `${day}/${month}/${year}`;
 }
+
+export const calcularDiasTranscurridos = (discount_date,payment_date) => { //yyyy-mm-dd
+
+    let fecha_de_descuento = new Date(discount_date);
+    let fecha_de_pago = new Date(payment_date);
+    let diff = ((fecha_de_pago-fecha_de_descuento)/(24 * 60 * 60 * 1000));
+    //console.log(`FDCTO: ${fecha_de_descuento} - FPAGO: ${fecha_de_pago} = DIFF: ${diff}`);
+    return diff;
+}
+
+export const calcularTasaEfectivaANDiasDeEfectiva = (dias_tasa,dias_transcurridos,rate_value) => {
+
+    rate_value /= 100;
+    return (Math.pow(1+rate_value,dias_transcurridos/dias_tasa)-1)*100;
+}
+
+//ToDo:
+export const calcularTasaEfectivaANDiasDeNominal = (dias_tasa,dias_capitalizacion,dias_transcurridos,valor_de_tasa) => {
+    var m = dias_tasa/dias_capitalizacion;
+    var n = dias_transcurridos/dias_capitalizacion;
+    return Math.pow(1+(valor_de_tasa/(m*100)),n)-1;
+}
+
+export const calcularTasaEfectivaDescuentoANDias = (tasa_efectiva_a_n_dias) => {
+    tasa_efectiva_a_n_dias /= 100;
+    return (tasa_efectiva_a_n_dias/(1+tasa_efectiva_a_n_dias))*100;
+}
+
+export const calcularDescuentoANDias = (tasa_efectiva_descuento_a_n_dias,nominal_value) =>{
+    tasa_efectiva_descuento_a_n_dias /= 100;
+    return tasa_efectiva_descuento_a_n_dias*nominal_value;
+}
+
+export const calcularValorNeto = (nominal_value,descuento_a_n_dias) =>{
+    return nominal_value-descuento_a_n_dias
+}
+
+//ToDo:
+export const calcularSumaCostosIniciales = (costos_iniciales) => {
+    var suma_costos_iniciales = 0;
+    costos_iniciales.forEach((value) => {
+        suma_costos_iniciales += value["cost"];
+    });
+    return suma_costos_iniciales;
+}
+
+export const calcularSumaCostosFinales = (costos_finales) => {
+    var suma_costos_finales = 0;
+    costos_finales.forEach((value) => {
+        suma_costos_finales += value["cost"];
+    });
+    return suma_costos_finales;
+}
+
+export const calcularValorRecibido = (valor_neto,suma_costos_iniciales,retention) =>{
+    return valor_neto-suma_costos_iniciales-retention;
+}
+
+export const calcularValorEntregado = (valor_nominal,suma_costos_finales,retention) =>{
+    return valor_nominal+suma_costos_finales-retention;
+}
+
+export const calcularTCEA = (valor_entregado,valor_recibido,dias_transcurridos,dias_por_anio) =>{
+    return Math.pow(valor_entregado/valor_recibido,dias_por_anio/dias_transcurridos)-1;
+}
